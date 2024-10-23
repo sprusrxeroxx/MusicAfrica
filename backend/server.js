f6cc5553fbd28b5658604cdc786a8fcd257238e4
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-23 09:33:39 +0000

    feat: main api route for songs

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/songs", (req, res) => {
+    res.send("Server is Ready!");
+});
+
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
 });
