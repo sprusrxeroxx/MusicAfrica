c555806f4d6a29cfed02fe9c588669293a8e7b92
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-26 21:59:16 +0000

    feat: created update endpoint + api debugging

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Song from "./models/song.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // Middleware : allows us to accept JSON data in the request body
+
+app.get("/api/songs", async (req, res) => {
+    try {
+        const songs = await Song.find({});
+        res.status(200).json({ success: true, data: songs });
+    } catch (error) {
+        console.log("Error in fetching songs:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+})
 
 app.post("/api/songs", async (req, res) => {
     const song = req.body; // user will request data to http body 
@@ -27,6 +38,23 @@ app.post("/api/songs", async (req, res) => {
     }
 });
 
+app.put("/api/songs/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const song = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return  res.status(404).json({ success:false, message: "Song Not Found" });
+    }
+
+    try {
+        const updatedSong= await Song.findByIdAndUpdate(id, song, {new:true});
+        res.status(200).json({ success:true, data: updatedSong });
+    } catch {
+        res.status(500).json({ success: false, message: "Server Error"});
+    }
+})
+
 app.delete("/api/songs/:id", async (req, res) => {
     const { id } = req.params;
     console.log("id:", id);
@@ -35,6 +63,7 @@ app.delete("/api/songs/:id", async (req, res) => {
         await Song.findByIdAndDelete(id);
         res.status(200).json({ success: true, message: "Song Deleted" });
     } catch (error) {
+        console.error("Error in deleting the song:", error.message);
         res.status(404).json({ success: false, message: "Song Not Found" });
     }
 });
