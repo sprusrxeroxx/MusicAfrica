18aaacde83f3517020064f98ae1ea8e2531ae34f
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-26 20:35:41 +0000

    feat: POST endpoint for song creation + console message for debugging

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import Song from "./models/song.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/songs", (req, res) => {});
+app.use(express.json()); // Middleware : allows us to accept JSON data in the request body
+
+app.post("/api/songs", async (req, res) => {
+    const song = req.body; // user will request data to http body 
+
+    if(!song.title || !song.artist || !song.audioUrl || !song.lyrics || !song.duration) {
+        return res.status(400).json({ success:false, message: "Please provide all fields!" });
+    }
+
+    const newSong = new Song(song);
+
+    try {
+        await newSong.save();
+        res.status(201).json({ success:true, data:newSong });
+    } catch (error) {
+        console.error("Error in Creating a Song:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
 
 app.listen(5000, () => {
     connectDB();
