21c238872f402097f19fa745b5774c634dc5c8ea
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-26 20:51:36 +0000

    fix: Fixed NameError on timestamps attribute

--- backend/models/song.model.js
+++ backend/models/song.model.js
@@ -1,29 +1,31 @@
 import mongoose from 'mongoose';
 
 const songSchema = new mongoose.Schema({ // Schema to dictate the structure of song info to be logged
-    title:{
-        type: String,
-        required: true
-    },
-    artist: {
-        type: String,
-        required: true
-    },
-    lyrics:{
-        type: String,
-        required: true
-    },
-    audioUrl:{
-        type: String,
-        required: true
-    },
-    duration:{
-        type: Number,
-        required: true
-    },
-},  {
-    timestamp: true // createdAt, updatedAt model logging
-});
+        title:{
+            type: String,
+            required: true
+        },
+        artist: {
+            type: String,
+            required: true
+        },
+        lyrics:{
+            type: String,
+            required: true
+        },
+        audioUrl:{
+            type: String,
+            required: true
+        },
+        duration:{
+            type: Number,
+            required: true
+        },
+    },  
+    {
+        timestamps: true, // createdAt, updatedAt model logging
+    }
+);
 
 const Song = mongoose.model('Song', songSchema);
 
