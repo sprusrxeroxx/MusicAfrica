9d2f26157539902a33be14575948508ed6aa5385
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-26 23:16:19 +0000

    styling: standardized port value as environment variable, not hardcoded

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,12 @@ import songRoutes from "./routes/song.route.js"
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000; 
 
 app.use(express.json()); // Middleware : allows us to accept JSON data in the request body
 app.use("/api/songs", songRoutes); // Links request calls to endpoints
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:"+PORT);
 });
