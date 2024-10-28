5fc536d8bb6d1e8c82be4bcc581c9a4d578ad510
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-28 22:56:29 +0000

    feat: added song add feature through client server

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Heading, Input, useColorModeValue, VStack, Container } from '@chakra-ui/react';
 import { useState } from 'react';
+import { useSongStore } from '../store/song';
 
 const CreatePage = () => {
     const [newSong, setNewSong] = useState({
@@ -9,60 +10,64 @@ const CreatePage = () => {
         audioUrl:"",
     });
 
-    const handleAddSong = () => {
-        console.log(newSong);
+    const {createSong}=useSongStore();
+
+    const handleAddSong = async() => {
+        const {success, message} = await createSong(newSong);
+        console.log("Success:", success);
+        console.log("Message:", message);
     };
 
-    return <Container maxW={"container.sm"}>
-        <VStack
-            spacing={8}
-        >
-            <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
-                Add New Song
-            </Heading>
-
-            <Box
-                w={"full"} bg={useColorModeValue("white", "gray.800")}
-                p={6} rounded={"lg"} shadow="md"
+    return (
+        <Container maxW={"container.sm"}>
+            <VStack
+                spacing={8}
             >
-                <VStack spacing={4}>
-                    <Input
-                        placeholder='Song Title'
-                        name='title'
-                        value={newSong.name}
-                        onChange={(e) => setNewSong({ ...newSong, name: e.target.value })}
-                    />
+                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+                    Add New Song
+                </Heading>
 
-                    <Input
-                        placeholder='Artist'
-                        name='artist'
-                        value={newSong.artist}
-                        onChange={(e) => setNewSong({ ...newSong, artist: e.target.value })}
-                    />
+                <Box
+                    w={"full"} bg={useColorModeValue("white", "gray.800")}
+                    p={6} rounded={"lg"} shadow="md"
+                >
+                    <VStack spacing={4}>
+                        <Input
+                            placeholder='Song Title'
+                            name='title'
+                            value={newSong.name}
+                            onChange={(e) => setNewSong({ ...newSong, title: e.target.value })}
+                        />
 
-                    <Input
-                        placeholder='Image URL'
-                        name='image'
-                        value={newSong.image}
-                        onChange={(e) => setNewSong({ ...newSong, image: e.target.value })}
-                    />
+                        <Input
+                            placeholder='Artist'
+                            name='artist'
+                            value={newSong.artist}
+                            onChange={(e) => setNewSong({ ...newSong, artist: e.target.value })}
+                        />
 
-                    <Input
-                        placeholder='Song URL'
-                        name='audioUrl'
-                        value={newSong.audioUrl}
-                        onChange={(e) => setNewSong({ ...newSong, audioUrl: e.target.value })}
-                    />
+                        <Input
+                            placeholder='Image URL'
+                            name='image'
+                            value={newSong.image}
+                            onChange={(e) => setNewSong({ ...newSong, image: e.target.value })}
+                        />
 
-                    <Button colorScheme='blue' onClick={handleAddSong} w="full">Add Song</Button>
+                        <Input
+                            placeholder='Song URL'
+                            name='audioUrl'
+                            value={newSong.audioUrl}
+                            onChange={(e) => setNewSong({ ...newSong, audioUrl: e.target.value })}
+                        />
 
-                </VStack>
-            </Box>
-
-
-
-        </VStack>
-    </Container>
-}
+                        <Button colorScheme='blue' onClick={handleAddSong} w="full">
+                            Add Song
+                        </Button>
+                    </VStack>
+                </Box>
+            </VStack>
+        </Container>
+    );
+};
 
 export default  CreatePage;
---
+++ frontend/src/store/song.js
@@ -0,0 +1,27 @@
+/**
+ * useSongStore: Creates a global state update for objects and stores it in the database
+ * set: A structure for mapping objects 
+ * returns: An object with updated state
+ */
+
+import {create} from 'zustand';
+
+export const useSongStore = create((set) => ({
+    songs: [],
+    setSongs: (songs) => set({ songs }),
+    createSong: async (newSong) => {
+        if(!newSong.artist || !newSong.title || !newSong.image || !newSong.audioUrl) {
+            return {success:false, message:"Please fill in all fields."}
+        }
+        const res = await fetch("/api/songs", {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify(newSong), // stringifies newObject and takes it as argument
+        });
+        const data = await res.json();
+        set((state) => ({ songs: [...state.songs, data.data] })); // Appends new song to end of list
+        return { success: true, message: "Song Added successfully." };
+    },
+}));
