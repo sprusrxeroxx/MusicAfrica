13f9c8066c6fb6f195370f7980fbf8331875f1ad
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-28 20:51:46 +0000

    feat: built createPage input fields, non-functional logging and styling

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,68 @@
+import { Box, Button, Heading, Input, useColorModeValue, VStack, Container } from '@chakra-ui/react';
+import { useState } from 'react';
+
 const CreatePage = () => {
-    return <div>CreatePage</div>
+    const [newSong, setNewSong] = useState({
+        title:"",
+        artist:"",
+        image:"",
+        audioUrl:"",
+    });
+
+    const handleAddSong = () => {
+        console.log(newSong);
+    };
+
+    return <Container maxW={"container.sm"}>
+        <VStack
+            spacing={8}
+        >
+            <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+                Add New Song
+            </Heading>
+
+            <Box
+                w={"full"} bg={useColorModeValue("white", "gray.800")}
+                p={6} rounded={"lg"} shadow="md"
+            >
+                <VStack spacing={4}>
+                    <Input
+                        placeholder='Song Title'
+                        name='title'
+                        value={newSong.name}
+                        onChange={(e) => setNewSong({ ...newSong, name: e.target.value })}
+                    />
+
+                    <Input
+                        placeholder='Artist'
+                        name='artist'
+                        value={newSong.artist}
+                        onChange={(e) => setNewSong({ ...newSong, artist: e.target.value })}
+                    />
+
+                    <Input
+                        placeholder='Image URL'
+                        name='image'
+                        value={newSong.image}
+                        onChange={(e) => setNewSong({ ...newSong, image: e.target.value })}
+                    />
+
+                    <Input
+                        placeholder='Song URL'
+                        name='audioUrl'
+                        value={newSong.audioUrl}
+                        onChange={(e) => setNewSong({ ...newSong, audioUrl: e.target.value })}
+                    />
+
+                    <Button colorScheme='blue' onClick={handleAddSong} w="full">Add Song</Button>
+
+                </VStack>
+            </Box>
+
+
+
+        </VStack>
+    </Container>
 }
 
 export default  CreatePage;
