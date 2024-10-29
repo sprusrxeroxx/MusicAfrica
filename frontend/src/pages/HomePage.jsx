72572cd9a5d09cc9c49207d07dfe6d2a20a5fdde
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-29 22:12:50 +0000

    feat: added Main components of HomePage + fixed hyperLink not working for Add A route

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,33 @@
+import { WarningIcon } from "@chakra-ui/icons/Warning";
+import { Container, VStack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-    return <div>HomePage</div>
-}
+    return (
+        <Container maxW='container.xl' py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize={"30"}
+                    fontWeight={"bold"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
+                    textAlign={"center"}
+                >
+                    Current Songs
+                </Text>
+                
+                
+                <Text fontSize='x1' textAlign={"center"} fontWeight='bold' color='gray.500'>
+                    No Songs found <WarningIcon/> {" "}
+                    <Link to={"/create"}>
+                        <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
+                            Add a Song
+                        </Text>
+                    </Link>
+                </Text>
+            </VStack>
+        </Container>
+    )
+};
 
 export default  HomePage;
