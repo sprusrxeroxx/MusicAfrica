00fabb9338416cbbe76d51771bf6095edd21f631
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-11-02 21:26:15 +0000

    feat: implemented session cookie making

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000; 
 
 app.use(express.json()); // Middleware : allows us to accept JSON data in the request body
+app.use(cookieParser());
 
 app.use("/api/songs", songRoutes); // Links song request calls to endpoints
 app.use("/api/users", userRoutes); // Links user request calls to endpoints
