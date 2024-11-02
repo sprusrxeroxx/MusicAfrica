8213a03777082ae89be73c29b5e2069a2afcd466
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-11-02 14:13:08 +0000

    refactor: commented out code for debbugging

---
+++ backend/context/AuthContext.jsx
@@ -0,0 +1,43 @@
+// import { createContext, useContext, useEffect, useState } from "react";
+// import {
+//     createUserWithEmailAndPassword,
+//     signInWithEmailAndPassword,
+//     signOut,
+//     onAuthStateChanged
+// } from 'firebase/auth';
+// import { auth } from '../firebase/config';
+
+// const AuthContext = createContext();
+
+// export const AuthProvider = ({ children }) => {
+//     const [ user, setUser ] = useState(null);
+//     const [ loading, setLoading ] = useState(true);
+
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, (user) => {
+//             setUser(user);
+//             setLoading(false);
+//         });
+//         return unsubscribe;
+//     }, []);
+
+//     const signup = ( email, password ) => {
+//         return createUserWithEmailAndPassword(auth, email, password);
+//     };
+
+//     const login = ( email, password ) => {
+//         return signInWithEmailAndPassword(auth, email, password);
+//     };
+
+//     const logout = () => {
+//         return signOut(auth);
+//     };
+
+//     return (
+//         <AuthContext.Provider value={{ user, signup, login, logout}}>
+//             {!loading && children}
+//         </AuthContext.Provider>
+//     );
+// };
+
+// export const useAuth = () => useContext(AuthContext);
