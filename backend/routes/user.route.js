ef0bbf7fdb3db36f9c23ece49580f17b64efa76b
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-12-11 00:57:26 +0000

    style: updated field parameter names

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -15,39 +15,19 @@ router.post("/login", FirebaseAuthController.loginUser);
 router.post("/logout", FirebaseAuthController.logoutUser);
 router.put("/:id", updateUser);
 
-
 // POEM ROUTES
 
 router.get("/poems", getAllPoems);
 
 router
-    .route("/:id/poems")
+    .route("/:uid/poems")
     .get(getPoem)
     .post(createPoem);
 
 router
-    .route("/:id/poems/:id")
+    .route("/:uid/poems/:pid")
     .delete(deletePoem)
     .put(updatePoem);
-
-
-
-
-
-// /api/users/               HOME FOR USER
-
-// POEM ROUTES
-//  api/users/poems             GET                     ALL POEMS 
-//  api/users/:id/poems         GET|POST                ALL POEMS BY USER | CREATE A POEM
-//  api/users/:id/poems/:id     GET|DEL|PUT             SPECIFIC POEM
-
-// USER ROUTES
-// api/users/:id            GET                     GET USER
-//  api/users/login         POST                    SIGN IN USER
-//  api/users/logout        POST                    SIGN OUT USER
-//  api/users/register      POST                    CREATE USER
-//  api/users/delete        POST                    DELETE USER
-// api/users/update         PUT                     UPDATE USER
 
 
 // router.delete("/:id", deleteUser);
